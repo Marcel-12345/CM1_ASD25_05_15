aacbd51ab0987766116a2d6e5d664f7c388778e9
Marcel-12345 <anselmus.marcel@gmail.com>
2025-04-16 10:46:47 +0700

    p

---
+++ CM1_ASD25_05_15/Bank.java
@@ -0,0 +1,20 @@
+package CM1_ASD25_05_15;
+
+public class Bank{
+    String noRekening;
+    String nama;
+    String namaIbu;
+    String noHp;
+    String email;
+
+    Bank(String noRekening, String nama, String namaIbu, String noHp, String email){
+        this.nama = nama;
+        this.namaIbu = namaIbu;
+        this.noHp = noHp;
+        this.email = email;
+    }
+
+    void tampilDataNorek(){
+        System.out.println(nama + "\t" + namaIbu + "\t" + noHp + "\t" + email);
+    }
+}
