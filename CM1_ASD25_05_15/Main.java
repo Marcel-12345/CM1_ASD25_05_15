5018f8449f568d4661c77494c8ad1add0da03b70
martaaa-16 <martaprama@gmail.com>
2025-04-16 11:16:33 +0700

    push

--- CM1_ASD25_05_15/Main.java
+++ CM1_ASD25_05_15/Main.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ServiceTransaksi data = null;
-        int pilihan;
+        Bank[] daftarBank = new Bank[5];
+        daftarBank[0] = new Bank("16030927 3084", "Wallace", "Mei Mei", "082-458-264-3263", "wallace@gmail.com");
+        daftarBank[1] = new Bank("16100167 0573", "Darius", "Susanti", "081-357-843-0547", "darius@pt.org");
+        daftarBank[2] = new Bank("16240401 2243", "Fuller", "Rosalia", "085-556-712-7062", "fuller@mail.com");
+        daftarBank[3] = new Bank("16270525 0112", "Maria", "Krabela", "082-267-223-0234", "maria@gmail.com");
+        daftarBank[4] = new Bank("16101617 2416", "Gery", "Fatimah", "083-683-416-8323", "gery@gery.com");
+
+        ServiceTransaksi data = new ServiceTransaksi(5);
+        data.add(new Transaksi("Tr005", 500000, 200000, 700000, "23-04-2024", "Kredit", daftarBank[4]));
+        data.add(new Transaksi("Tr004", 450000, 150000, 650000, "23-04-2024", "Kredit", daftarBank[3]));
+        data.add(new Transaksi("Tr003", 350000, 300000, 200000, "23-04-2024", "Debit", daftarBank[2]));
+        data.add(new Transaksi("Tr002", 200000, 100000, 600000, "23-04-2024", "Kredit", daftarBank[1]));
+        data.add(new Transaksi("Tr001", 300000, 100000, 400000, "23-04-2024", "Debit", daftarBank[0]));
+
+        int pilih;
 
         while (true) {
             System.out.println("===================== POLINEMA BANK ====================");
@@ -21,25 +34,24 @@ public class Main {
 
             switch (pilih) {
                 case 1:
-
+                    for (Bank b : daftarBank) {
+                        b.tampilDataNorek();
+                        System.out.println();
+                    }
                     break;
                 case 2:
-
+                    data.displayData();
                     break;
                 case 3:
-
+                    System.out.print("Input Email: ");
+                    String email = sc.nextLine();
+                    data.Seacrhing(email);
                     break;
                 case 4:
-
+                    data.findMinMax();
                     break;
                 case 5:
-                    if (data != null) {
-                        data.sortBySaldoAsc();
-                        System.out.println(" ");
-                        data.tampil();
-                    } else {
-                        System.out.println("Belum ada data.");
-                    }
+                    data.Sorting();
                     break;
                 case 6:
                     System.out.println("Keluar.");
--- CM1_ASD25_05_15/ServiceTransaksi.java
+++ CM1_ASD25_05_15/ServiceTransaksi.java
@@ -1,15 +1,15 @@
 public class ServiceTransaksi {
-    Transaksi[] Trs;
+    Transaksi[] Trans;
     int index;
 
     public ServiceTransaksi(int kapasitas) {
-        Trs = new Transaksi[kapasitas];
+        Trans = new Transaksi[kapasitas];
         index = 0;
     }
 
     public void tambah(Transaksi t) {
-        if (index < Trs.length) {
-            Trs[index] = t;
+        if (index < Trans.length) {
+            Trans[index] = t;
             index++;
         } else {
             System.out.println("Data sudah penuh!");
@@ -18,57 +18,53 @@ public class ServiceTransaksi {
 
     public void displayData() {
         for (int i = 0; i < index; i++) {
-            Trs[i].tampilDataTransaksi();
+            Trans[i].tampilDataTransaksi();
             System.out.println("--------------------");
         }
     }
 
-    public Transaksi searchByKode(String kode) {
-        for (int i = 0; i < index; i++) {
-            if (Trs[i].kodeTransaksi.equals(kode)) {
-                return Trs[i];
+    void Seacrhing(String email) {
+        boolean temukan = false;
+        for (Transaksi t : Trans) {
+            if (t != null && t.bankAcc.email.equalsIgnoreCase(email)) {
+                t.tampilDataTransaksi();
+                temukan = true;
             }
         }
-        return null;
+        if (!temukan) {
+            System.out.println("Transaksi dengan email " + email + " tidak ditemukan.");
+        }
     }
 
-    public void findMinInOutByType(String tipe) {
-        Transaksi min = null;
-        for (int i = 0; i < index; i++) {
-            if (Trs[i].type.equalsIgnoreCase(tipe)) {
-                if (min == null || Trs[i].inOutSaldo < min.inOutSaldo) {
-                    min = Trs[i];
+    void findMinMax() {
+        if (Trans.length == 0 || Trans[0] == null) {
+            System.out.println("Tidak ada data transaksi.");
+            return;
+        }
+
+        Transaksi max = Trans[0];
+        for (Transaksi t : Trans) {
+            if (t != null && t.inOutSaldo > max.inOutSaldo) {
+                max = t;
+            }
+        }
+
+        System.out.println("Menampilkan Transaksi dengan Debit/Kredit Tertinggi:");
+        max.tampilDataTransaksi();
+    }
+
+    void Sorting() {
+        for (int i = 0; i < Trans.length - 1; i++) {
+            for (int j = 0; j < Trans.length - i - 1; j++) {
+                if (Trans[j] != null && Trans[j + 1] != null && Trans[j].inOutSaldo > Trans[j + 1].inOutSaldo) {
+                    Transaksi temp = Trans[j];
+                    Trans[j] = Trans[j + 1];
+                    Trans[j + 1] = temp;
                 }
             }
         }
-        if (min != null) {
-            System.out.println("Transaksi " + tipe + " dengan nilai terkecil:");
-            min.tampilDataTransaksi();
-        } else {
-            System.out.println("Tidak ada transaksi dengan tipe: " + tipe);
-        }
-    }
 
-    public Transaksi findMaxSaldo() {
-        if (index == 0) return null;
-        Transaksi max = Trs[0];
-        for (int i = 1; i < index; i++) {
-            if (Trs[i].saldo > max.saldo) {
-                max = Trs[i];
-            }
-        }
-        return max;
-    }
-
-    public void sortBySaldoAsc() {
-        for (int i = 0; i < index - 1; i++) {
-            for (int j = 0; j < index - i - 1; j++) {
-                if (Trs[j].saldo > Trs[j + 1].saldo) {
-                    Transaksi temp = Trs[j];
-                    Trs[j] = Trs[j + 1];
-                    Trs[j + 1] = temp;
-                }
-            }
-        }
+        System.out.println("Transaksi diurutkan berdasarkan Debit/Kredit ASC:");
+        displayData();
     }
 }
